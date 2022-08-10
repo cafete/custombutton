393d4042b7ad4694e3fcc518b98a53c48c8e63f8
Joaquín Alonso <28913157+cafete@users.noreply.github.com>
2022-08-10 10:17:51 -0500

    feat: added reusablebutton component

--- reusablebutton/src/main/java/example/reusablebutton/MainActivity.kt
+++ reusablebutton/src/main/java/example/reusablebutton/MainActivity.kt
@@ -1,43 +1,42 @@
 package example.reusablebutton
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import example.reusablebutton.ui.theme.ReusableButtonTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ReusableButtonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReusableButton(
+    name: String = "amigo", size: String = "md", buttonText: String = "Saludos") {
+    val ctx = LocalContext.current
+    var w = 0
+    var h = 0
+    when (size) {
+        "xl" -> {w = 400; h = 200}
+        "lg" -> {w = 300; h = 150}
+        "md" -> {w = 200; h = 100}
+        "sm" -> {w = 150; h = 70}
+        "xs" -> {w = 100; h = 50}
+    }
+    Button(
+        modifier = Modifier.width(w.dp).height(h.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray),
+        onClick = {
+            Toast.makeText(ctx, "¡Felicidades ${name}!", Toast.LENGTH_SHORT).show()
+        }) {
+        Text(buttonText, color = Color.White)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ReusableButtonTheme() {
-        Greeting("Android")
+    ReusableButtonTheme {
     }
 }
