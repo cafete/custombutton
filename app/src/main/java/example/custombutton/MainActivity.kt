8daa23136c8604f0ab17681bdd806191ed97aa83
Joaquín Alonso <28913157+cafete@users.noreply.github.com>
2022-08-10 10:26:35 -0500

    feat: implemented custombutton app

--- app/src/main/java/example/custombutton/MainActivity.kt
+++ app/src/main/java/example/custombutton/MainActivity.kt
@@ -3,26 +3,27 @@ package example.custombutton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import example.custombutton.ui.theme.CustomButtonTheme
+import example.reusablebutton.ReusableButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CustomButtonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(200.dp)) {
+                    Greeting("compañero")
                 }
             }
         }
@@ -31,13 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    ReusableButton(name, "md", "¡Pulse Aquí!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CustomButtonTheme() {
+    CustomButtonTheme {
         Greeting("Android")
     }
 }
